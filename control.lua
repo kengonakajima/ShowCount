b1b6e98fed647727617c9c0835bddc2ce78d54e3
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-04-03 03:38:38 +0900

    on_init fix

--- control.lua
+++ control.lua
@@ -18,13 +18,16 @@ script.on_event(defines.events.on_tick, function(event)
     local swt_cnt = game.player.force.get_kill_count( "small-worm-turret" )
     local mwt_cnt = game.player.force.get_kill_count( "medium-worm-turret" )    
     local bwt_cnt = game.player.force.get_kill_count( "big-worm-turret" )
-    
+
+    if game.player.gui.top.countdisplay == nil then
+    	game.player.gui.top.add{ name = "countdisplay", type = "label", caption = "Counts", style="caption_label_style"}
+    end
     game.player.gui.top.countdisplay.caption = "Biters: " .. tostring(bhb_cnt) .. "/" .. tostring(bb_cnt) .. "/" .. tostring(mb_cnt) .. "/" .. tostring(ss_cnt) .. " Spitters: " .. tostring(bhs_cnt) .. "/" .. tostring(bs_cnt) .. "/" .. tostring(ms_cnt) .. "/" .. tostring(ss_cnt) .. " Turrets: " .. tostring(bwt_cnt) .. "/" .. tostring(mwt_cnt) .. "/" .. tostring(swt_cnt)
 end)
 
 script.on_init( function(event)    
 	-- GUI
-	game.player.gui.top.add{ name = "countdisplay", type = "label", caption = "Counts", style="caption_label_style"}
+
 end)
 
 script.on_event(defines.events.on_gui_click, function(event)
